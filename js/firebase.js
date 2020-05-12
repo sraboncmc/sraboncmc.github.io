d61b619e1e122e354c6f90d29978ac64d5729765
sraboncmc <dhongu2009@gmail.com>
2020-05-13 03:44:42 +0600

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+
+
+
+function openImage(image)
+{
+    if (image.indexOf(".gif") >= 0) {
+       myExternal("external=external/view-image.php?pstring="+image+"&title=View Image");
+    }
+}
+
+
+
+// Always listen...
